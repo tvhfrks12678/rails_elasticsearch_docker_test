e6e9d6004597ad326aab39768c0ebd86494db852
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-12-14 17:21:20 +0900

    build: add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,21 @@ ActiveRecord::Base.transaction do
         body: "記事本文_#{index}"
     )
   end
+
+  Article.create(
+    title: "犬の気持ち",
+    body: "吾輩は犬である。名前はまだない。",
+  )
+  
+  Article.create(
+    title: "ねこのすべて",
+    body: "猫はとても気まぐれです。気まぐれロマンティック。構って欲しい時にしか寄ってきません。ぴえん。",
+  )
+  
+  Article.create(
+    title: "私VTuberになる",
+    body: "どうも初めましてVTuberの酢飯マグロです。VTuberを始めて1年経って分かった5つの大事な事を紹介します。",
+  )
+
 end
 
